e6bcf1982f7cad856e3e5d2cfaa38fb7c0225715
Muflih-uk <muhammedmuflih875@gmail.com>
2026-07-11 10:01:11 +0530

    [feat]: create api's for courses

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("apps.accounts.urls")),
-    path("api/courses/", include("apps.courses.urls")),
+    path("api/", include("apps.courses.urls")),
     path("api/enrollments/", include("apps.enrollments.urls")),
     # path("api/inventory/", include("apps.inventory.urls")),
     # path("api/workshops/", include("apps.workshops.urls")),
