dfea397da7cd2fde798c2b4f9db62615c9fcd943
Muflih-uk <muhammedmuflih875@gmail.com>
2026-07-18 19:36:29 +0530

    [feat]: add inventory and course to admin panel

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,94 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Announcement,
+    Course,
+    CourseFeedback,
+    CourseMaterial,
+)
+
+
+class CourseMaterialInline(admin.TabularInline):
+    model = CourseMaterial
+    extra = 1
+
+
+class AnnouncementInline(admin.TabularInline):
+    model = Announcement
+    extra = 1
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "level",
+        "price",
+        "is_published",
+        "created_by",
+        "created_at",
+    )
+    list_filter = ("level", "is_published", "created_at")
+    search_fields = ("title", "description")
+    readonly_fields = ("id", "created_at", "updated_at")
+    inlines = [CourseMaterialInline, AnnouncementInline]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(CourseMaterial)
+class CourseMaterialAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "course",
+        "type",
+        "created_by",
+        "created_at",
+    )
+    list_filter = ("type", "created_at")
+    search_fields = ("title", "course__title")
+    readonly_fields = ("id", "created_at")
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "course",
+        "created_by",
+        "created_at",
+    )
+    list_filter = ("created_at",)
+    search_fields = ("title", "content", "course__title")
+    readonly_fields = ("id", "created_at")
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(CourseFeedback)
+class CourseFeedbackAdmin(admin.ModelAdmin):
+    list_display = (
+        "course",
+        "student",
+        "rating",
+        "is_published",
+        "created_at",
+    )
+    list_filter = ("rating", "is_published", "created_at")
+    search_fields = (
+        "course__title",
+        "student__username",
+        "comment",
+    )
+    readonly_fields = ("id", "created_at", "updated_at")
--- apps/workshops/admin.py
+++ apps/workshops/admin.py
@@ -1,3 +1,106 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    GalleryImage,
+    Workshop,
+    WorkshopRegistration,
+)
+
+
+class GalleryImageInline(admin.TabularInline):
+    model = GalleryImage
+    extra = 1
+
+
+class WorkshopRegistrationInline(admin.TabularInline):
+    model = WorkshopRegistration
+    extra = 0
+    readonly_fields = ("created_at",)
+
+
+@admin.register(Workshop)
+class WorkshopAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "status",
+        "event_date",
+        "location",
+        "is_published",
+        "created_by",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "is_published",
+        "event_date",
+        "created_at",
+    )
+    search_fields = (
+        "title",
+        "description",
+        "location",
+    )
+    readonly_fields = (
+        "id",
+        "created_at",
+        "updated_at",
+    )
+    inlines = [
+        GalleryImageInline,
+        WorkshopRegistrationInline,
+    ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(GalleryImage)
+class GalleryImageAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "workshop",
+        "uploaded_by",
+        "created_at",
+    )
+    list_filter = (
+        "workshop",
+        "created_at",
+    )
+    search_fields = (
+        "title",
+        "workshop__title",
+    )
+    readonly_fields = (
+        "id",
+        "created_at",
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not obj.uploaded_by:
+            obj.uploaded_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(WorkshopRegistration)
+class WorkshopRegistrationAdmin(admin.ModelAdmin):
+    list_display = (
+        "workshop",
+        "student",
+        "status",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "created_at",
+    )
+    search_fields = (
+        "workshop__title",
+        "student__username",
+        "student__email",
+    )
+    readonly_fields = (
+        "id",
+        "created_at",
+    )
