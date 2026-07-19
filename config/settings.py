e29f302311f53db850eb83d594d0f20368d7229a
Muflih-uk <muhammedmuflih875@gmail.com>
2026-07-19 09:33:53 +0530

    [fix]: add cors header

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_restframework",
+    "corsheaders",
     "apps.accounts",
     "apps.courses",
     "apps.enrollments",
@@ -38,6 +39,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -119,3 +121,12 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://your-frontend.vercel.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True
